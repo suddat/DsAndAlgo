180287de5b4d8b43106197306726554fec6986a4
suddat <suddat.shende@moneyonclick.com>
2023-10-08 14:04:40 +0530

    1 to N and N to 1 with recurrsion

---
+++ src/com/ds/algo/AtoZStriver/basicRecursion/SumFirstN.java
@@ -0,0 +1,135 @@
+package com.ds.algo.AtoZStriver.basicRecursion;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.lang.System.in;
+
+public class SumFirstN {
+    static PrintWriter out = new PrintWriter((System.out));
+
+    public static void main(String[] args) throws IOException {
+        FastReader sc = new FastReader();
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            solve(sc);
+        }
+        out.close();
+    }
+
+    public static void solve(FastReader rc) {
+        long n = rc.nextInt();
+
+        long ans = reverseArray(n);
+
+        System.out.println(ans);
+    }
+
+    private static long reverseArray(long n) {
+        if(n <= 0){
+            return 0;
+        }
+
+        n += reverseArray(n-1);
+
+        return n;
+    }
+
+    private static void swap(int p1, int p2, int[] arr) {
+        int t = arr[p1];
+        arr[p1] = arr[p2];
+        arr[p2] = t;
+    }
+
+    private static void printArray(int[] arr){
+
+        for (int i : arr){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+    }
+
+    private static void print2DArray(int[][] arr){
+        for (int[] a : arr){
+            for (int i : a){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static class FastReader {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        StringTokenizer st = new StringTokenizer("");
+
+        public String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return parseInt(next());
+        }
+
+        public long nextLong() {
+            return parseLong(next());
+        }
+
+        public double nextDouble() {
+            return parseDouble(next());
+        }
+
+        public String nextLine() {
+            try {
+                return br.readLine();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        public boolean hasNext() {
+            String next = null;
+            try {
+                next = br.readLine();
+            } catch (Exception e) {
+            }
+            if (next == null) {
+                return false;
+            }
+            st = new StringTokenizer(next);
+            return true;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+}
+
