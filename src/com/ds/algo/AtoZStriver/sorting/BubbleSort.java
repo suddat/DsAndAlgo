1abfea9ece57d8342bda5ebf997df725c99e9321
suddat <suddat.shende@moneyonclick.com>
2023-10-09 15:39:35 +0530

    Basic Sort

---
+++ src/com/ds/algo/AtoZStriver/sorting/BubbleSort.java
@@ -0,0 +1,143 @@
+package com.ds.algo.AtoZStriver.sorting;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.lang.System.in;
+
+public class BubbleSort {
+    static PrintWriter out = new PrintWriter((System.out));
+
+    public static void main(String[] args) throws IOException {
+        FastReader sc = new FastReader();
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            solve(sc);
+        }
+        out.close();
+    }
+
+    public static void solve(FastReader rc) {
+        int n = rc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = rc.nextInt();
+        }
+
+        solution(n, arr);
+        printArray(arr);
+    }
+
+    private static void solution(int n, int[] nums) {
+        if(nums.length <= 0)
+            return;
+
+        for(int i = n-1 ; i > 0 ; i--){
+            int didSwap = 0;
+            for(int j = 0 ; j < i ; j++){
+                if(nums[j] > nums[j+1]){
+                    swap(j+1, j, nums);
+                    didSwap = 1;
+                }
+            }
+            if(didSwap == 0) {
+                break;
+            }
+            System.out.println("break the statement");
+        }
+    }
+
+    private static void swap(int p1, int p2, int[] arr) {
+        int t = arr[p1];
+        arr[p1] = arr[p2];
+        arr[p2] = t;
+    }
+
+    private static void printArray(int[] arr) {
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+    }
+
+    private static void print2DArray(int[][] arr) {
+
+    }
+
+    static class FastReader {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        StringTokenizer st = new StringTokenizer("");
+
+        public String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return parseInt(next());
+        }
+
+        public long nextLong() {
+            return parseLong(next());
+        }
+
+        public double nextDouble() {
+            return parseDouble(next());
+        }
+
+        public String nextLine() {
+            try {
+                return br.readLine();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        public boolean hasNext() {
+            String next = null;
+            try {
+                next = br.readLine();
+            } catch (Exception e) {
+            }
+            if (next == null) {
+                return false;
+            }
+            st = new StringTokenizer(next);
+            return true;
+        }
+    }
+
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+}
+
