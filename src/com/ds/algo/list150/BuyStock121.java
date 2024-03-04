a25ce759928285cdadd58c77d6861f5505dc5dbb
suddat <suddat.shende@moneyonclick.com>
2024-03-04 20:21:17 +0530

    practice 150 March 4 evening

---
+++ src/com/ds/algo/list150/BuyStock121.java
@@ -0,0 +1,144 @@
+package com.ds.algo.list150;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.*;
+import static java.lang.Long.parseLong;
+import static java.lang.System.in;
+
+public class BuyStock121 {
+    static PrintWriter out = new PrintWriter((System.out));
+
+    public static void main(String[] args) throws IOException {
+        FastReader sc = new FastReader();
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            solve(sc);
+        }
+        out.close();
+    }
+
+    public static void solve(FastReader rc) {
+//        int[] nums = {1,1,1,2,2,3};
+//        int[] nums = {7,1,5,3,6,4};
+        int[] nums = {7,6,4,3,1};
+        int n = nums.length;
+        int overallMax = 0;
+        int cnt=0;
+        for(int i = 1 ; i < n ; i++){
+            int currMax = nums[i] - nums[cnt];
+            if(currMax <= 0){
+                cnt = i;
+            }else{
+                overallMax = Math.max(currMax, overallMax);
+            }
+        }
+        System.out.println(overallMax);
+    }
+
+    private static void reverse(int[] nums, int k, int n) {
+        while(k < n){
+            swap(k , n, nums);
+            k++;
+            n--;
+        }
+    }
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+    }
+
+    private static void printArray(int[] arr) {
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+    }
+
+    private static void swap(int p1, int p2, char[] arr) {
+        char t = arr[p1];
+        arr[p1] = arr[p2];
+        arr[p2] = t;
+    }
+
+    private static void swap(int p1, int p2, int[] arr) {
+        int t = arr[p1];
+        arr[p1] = arr[p2];
+        arr[p2] = t;
+    }
+
+    static class FastReader {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        StringTokenizer st = new StringTokenizer("");
+
+        public String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return parseInt(next());
+        }
+
+        public long nextLong() {
+            return parseLong(next());
+        }
+
+        public double nextDouble() {
+            return parseDouble(next());
+        }
+
+        public String nextLine() {
+            try {
+                return br.readLine();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        public boolean hasNext() {
+            String next = null;
+            try {
+                next = br.readLine();
+            } catch (Exception e) {
+            }
+            if (next == null) {
+                return false;
+            }
+            st = new StringTokenizer(next);
+            return true;
+        }
+    }
+
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+}
