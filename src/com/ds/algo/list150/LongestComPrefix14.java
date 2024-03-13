923d3a4b9f29998b6954b744b8fef42072ad70f4
suddat <suddat.shende@moneyonclick.com>
2024-03-13 09:08:30 +0530

    Mar 12 commits

---
+++ src/com/ds/algo/list150/LongestComPrefix14.java
@@ -0,0 +1,149 @@
+package com.ds.algo.list150;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.lang.System.in;
+
+public class LongestComPrefix14 {
+    static PrintWriter out = new PrintWriter((System.out));
+
+    public static void main(String[] args) throws IOException {
+        FastReader sc = new FastReader();
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            solve(sc);
+        }
+        out.close();
+    }
+
+    public static void solve(FastReader rc) {
+        String[] strs = {"flower"};
+        String ans = "";
+        boolean matching = true;
+        int lengthOfSmallestString = getSmallestStringLength(strs);
+        System.out.println(lengthOfSmallestString);
+        for(int i = 0 ; i<lengthOfSmallestString; i++){
+            char temp = strs[0].charAt(i);
+            for(int j = 1 ; j < strs.length; j++){
+                if(strs[j].charAt(i) != temp){
+                    matching = false;
+                    break;
+                }
+            }
+            if(!matching){
+                break;
+            }else{
+                ans = ans+temp;
+            }
+        }
+        System.out.println(ans);
+    }
+
+    private static int getSmallestStringLength(String[] strs) {
+        int length = strs[0].length();
+        for(int i = 1; i < strs.length; i++){
+            length = Math.min(length, strs[i].length());
+        }
+        return length;
+    }
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+    }
+
+    private static void printArray(int[] arr) {
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+    }
+
+    private static void swap(int p1, int p2, char[] arr) {
+        char t = arr[p1];
+        arr[p1] = arr[p2];
+        arr[p2] = t;
+    }
+
+    private static void swap(int p1, int p2, int[] arr) {
+        int t = arr[p1];
+        arr[p1] = arr[p2];
+        arr[p2] = t;
+    }
+
+    static class FastReader {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        StringTokenizer st = new StringTokenizer("");
+
+        public String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return parseInt(next());
+        }
+
+        public long nextLong() {
+            return parseLong(next());
+        }
+
+        public double nextDouble() {
+            return parseDouble(next());
+        }
+
+        public String nextLine() {
+            try {
+                return br.readLine();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        public boolean hasNext() {
+            String next = null;
+            try {
+                next = br.readLine();
+            } catch (Exception e) {
+            }
+            if (next == null) {
+                return false;
+            }
+            st = new StringTokenizer(next);
+            return true;
+        }
+    }
+
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+}
